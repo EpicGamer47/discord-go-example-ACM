1eaba1ded18817bedc2ad3512069242600a9fbcf
Chris B <clb46@njit.edu>
2025-11-08 21:27:43 -0500

    Finalized pretty bot syntax

--- go.mod
+++ go.mod
@@ -2,11 +2,16 @@ module my-discord-bot
 
 go 1.25.4
 
-require github.com/bwmarrin/discordgo v0.29.0
+require (
+	github.com/bwmarrin/discordgo v0.29.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lus/dgc v1.1.0
+)
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/karrick/tparse/v2 v2.8.1 // indirect
+	github.com/zekroTJA/timedmap v0.0.0-20200518230343-de9b879d109a // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
 )
--- ping/main.go
+++ ping/main.go
@@ -1,4 +1,4 @@
-package ping
+package main
 
 import (
 	"fmt"
@@ -9,7 +9,41 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
+	"github.com/lus/dgc"
 )
+
+// slash commands
+var commands = []*discordgo.ApplicationCommand{
+	{
+		Name:        "ping",
+		Description: "Responds with Pong!",
+	},
+}
+var slashCommandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+	"ping": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Pong!",
+			},
+		})
+	},
+}
+
+// text commands
+func registerCommands(router *dgc.Router) {
+
+	router.RegisterCmd(&dgc.Command{
+		Name:        "ping",
+		Description: "Responds with pong",
+		Handler:     pingHandler,
+		IgnoreCase:  true,
+	})
+}
+
+func pingHandler(ctx *dgc.Ctx) {
+	ctx.RespondText("Pong!")
+}
 
 func main() {
 	err := godotenv.Load()
@@ -17,10 +51,17 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	Token := os.Getenv("DISCORD_TOKEN")
+	Token := os.Getenv("DISCORD_BOT_TOKEN")
+	// NOTE: Changed "DISCORD_TOKEN" to "DISCORD_BOT_TOKEN" to match common practice
 	if Token == "" {
-		fmt.Println("Error: DISCORD_TOKEN not found in .env file.")
-		return
+		log.Fatal("Error: DISCORD_BOT_TOKEN not found in .env file.")
+	}
+
+	guildID := os.Getenv("TEST_GUILD_ID")
+	if guildID == "" {
+		log.Println("WARNING: TEST_GUILD_ID not found in .env. Registering commands globally (takes up to 1 hour).")
+	} else {
+		log.Printf("INFO: Registering commands to Guild ID: %s (instant update)", guildID)
 	}
 
 	dg, err := discordgo.New("Bot " + Token)
@@ -29,9 +70,27 @@ func main() {
 		return
 	}
 
-	dg.AddHandler(messageCreate)
+	router := dgc.Create(&dgc.Router{
+		Prefixes: []string{"!"},
+	})
 
-	dg.Identify.Intents = discordgo.IntentsGuildMessages
+	registerCommands(router)
+
+	dg.AddHandler(router.Handler())
+
+	dg.Identify.Intents = (discordgo.IntentsGuilds |
+		discordgo.IntentsGuildMessages |
+		discordgo.IntentsMessageContent)
+
+	dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if i.Type == discordgo.InteractionApplicationCommand {
+			if handler, ok := slashCommandHandlers[i.ApplicationCommandData().Name]; ok {
+				handler(s, i)
+			}
+		}
+	})
+
+	registeredCommands := make(map[string]string, len(commands))
 
 	err = dg.Open()
 	if err != nil {
@@ -39,24 +98,31 @@ func main() {
 		return
 	}
 
-	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	log.Println("Bot is now running. Registering slash commands...")
+
+	for _, v := range commands {
+		cmd, err := dg.ApplicationCommandCreate(dg.State.User.ID, guildID, v)
+		if err != nil {
+			log.Fatalf("Cannot create command %s: %v", v.Name, err)
+		}
+		registeredCommands[cmd.Name] = cmd.ID
+	}
+
+	fmt.Println("Commands registered.")
+	fmt.Println("Press CTRL-C to exit and automatically clean up slash commands.")
+
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 
+	log.Println("Shutting down gracefully...")
+	for name, id := range registeredCommands {
+		err := dg.ApplicationCommandDelete(dg.State.User.ID, guildID, id)
+		if err != nil {
+			log.Printf("Cannot delete command %s: %v", name, err)
+		}
+	}
+
 	dg.Close()
-}
-
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
-	}
-
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
-	}
+	log.Println("Shutdown complete.")
 }
